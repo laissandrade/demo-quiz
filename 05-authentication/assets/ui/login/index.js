6ba47ae1e3ee8f3bee4abe50d53efb4dc13a20cd
Lais Andrade <lais.andrade@liferay.com>
2016-11-11 19:43:58 -0300

    Adds 05-authentication assets and final folders.

---
+++ 05-authentication/assets/ui/login/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const DOMAIN = window.location.hostname.split(".").slice(-3).join(".");
+
+function signInWithGithub() {
+  return false;
+}
+
+function signInWithGoogle() {
+  return false;
+}
